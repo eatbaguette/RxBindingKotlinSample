c43c1539fb8d076cf93a3a6cf88689fff574bf91
gikeda <gikeda@appirits.com>
2018-01-31 16:53:49 +0900

    [add] 実装

--- app/src/main/java/com/example/baguette/rxbindingkotlinsample/MainActivity.kt
+++ app/src/main/java/com/example/baguette/rxbindingkotlinsample/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.baguette.rxbindingkotlinsample
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.baguette.rxbindingkotlinsample.databinding.ActivityMainBinding
+import com.jakewharton.rxbinding.widget.RxTextView
 
 class MainActivity : AppCompatActivity() {
 
+    val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val viewModel = ViewModel(RxTextView.textChanges(binding.editText))
+        binding.viewModel = viewModel
     }
 }
---
+++ app/src/main/java/com/example/baguette/rxbindingkotlinsample/ViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.baguette.rxbindingkotlinsample
+
+import jp.keita.kagurazaka.rxproperty.ReadOnlyRxProperty
+
+/**
+ * Created by monkey on 2018/01/31.
+ */
+class ViewModel(textChangeStream: rx.Observable<CharSequence>) {
+    val text: ReadOnlyRxProperty<String>
+
+    val upperCase: ReadOnlyRxProperty<String>
+    val fileterI: ReadOnlyRxProperty<String>
+
+    init {
+        val textStream = textChangeStream
+                .publish()
+                .refCount()
+
+        text = textStream
+                .map(CharSequence::toString)
+                .toRxProperty()
+
+        upperCase = textStream
+                .map(CharSequence::toString)
+                .map { it.toUpperCase() }
+                .toRxProperty()
+
+        fileterI = textStream
+                .map(CharSequence::toString)
+                .map { it.replace("apple", "", ignoreCase = true) }
+                .toRxProperty()
+    }
+}
